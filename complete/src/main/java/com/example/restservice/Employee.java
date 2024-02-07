9f90df4ed4b29800faa9d7f99cb4d7216eebd647
yeek1ATWIT <yeek1@wit.edu>
2024-02-06 20:20:11 -0500

    Update Employee.java

--- complete/src/main/java/com/example/restservice/Employee.java
+++ complete/src/main/java/com/example/restservice/Employee.java
@@ -36,4 +36,26 @@ public class Employee {
     public String getTitle() {
         return title;
     }
+
+    // Setter methods
+    public void setEmployee_id(String employee_id) {
+        this.employee_id = employee_id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
+
